e548a75003549e56260f882c35bc5f23cbda5267
Lorena C <criado.manzaneque@gmail.com>
2024-04-30 17:55:37 +0200

    Create html section where showing with JS the result

--- 3-Obtener info según el tipo de mascota y edad/app.js
+++ 3-Obtener info según el tipo de mascota y edad/app.js
@@ -1,43 +1,45 @@
 var pet;
 var age;
+var result;
 
 function getPetExerciseInfo(pet, age) {
     pet = document.getElementById('pet-input').value
     age = document.getElementById('age-input').value
+    result = document.getElementById('info-container')
 
     switch (pet) {
         case "perro":
             if (age < 1) {
-                console.log("Los cachorros necesitan pequeñas y frecuentes sesiones de juego")
+                result.innerText = "Los cachorros necesitan pequeñas y frecuentes sesiones de juego"
             } else if (age >= 1 && age <= 7) {
-                console.log("Los perros a esta edad necesitan caminatas diarias y sesiones de juego")
+                result.innerText = "Los perros a esta edad necesitan caminatas diarias y sesiones de juego"
             } else if (age > 7) {
-                console.log("Los perros adultos necesitan caminatas más cortas")
+                result.innerText = "Los perros adultos necesitan caminatas más cortas"
             }
             break;
 
         case "gato":
             if (age < 1) {
-                console.log("Los gatitos necesitan frecuentes sesiones de juego")
+                result.innerText = "Los gatitos necesitan frecuentes sesiones de juego"
             } else if (age >= 1 && age <= 7) {
-                console.log("Los gatos a esta edad necesitan jugar diariamente")
+                result.innerText = "Los gatos a esta edad necesitan jugar diariamente"
             } else if (age > 7) {
-                console.log("Los gatos adultos necesitan sesiones de juego más cortas")
+                result.innerText = "Los gatos adultos necesitan sesiones de juego más cortas"
             }
             break;
 
         case "ave":
             if (age < 1) {
-                console.log("Las aves jóvenes necesitan mucho espacio para volar")
+                result.innerText = "Las aves jóvenes necesitan mucho espacio para volar"
             } else if (age >= 1 && age <= 7) {
-                console.log("Las aves necesitan jugar diariamente y un lugar para volar")
+                result.innerText = "Las aves necesitan jugar diariamente y un lugar para volar"
             } else if (age > 7) {
-                console.log("Las aves adultas necesitan descansar más, pero siguen ocupando un lugar para volar")
+                result.innerText = "Las aves adultas necesitan descansar más, pero siguen ocupando un lugar para volar"
             }
             break;
 
         default:
-            console.log("Lo sentimos, no tenemos información sobre este tipo de mascota")
+            result.innerText = "Lo sentimos, no tenemos información sobre este tipo de mascota"
     }
 
 }
